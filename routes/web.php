0c46220985bcee66667a03a9d9bbefa140e77295
Your Name <nihaal@sapnagroup.com>
2024-01-21 23:17:24 +0530

    Model and reading data

--- routes/web.php
+++ routes/web.php
@@ -71,21 +71,18 @@ Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
+        'tasks' => \App\Models\Task::latest()->get()
     ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use($tasks) {
+Route::get('/tasks/{id}', function ($id) {
 
-    $task = collect($tasks)->firstWhere('id', $id);
+  return view('show', [
+    'task' => \App\Models\Task::findOrFail($id)
+  ]);
 
-    if(!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
-
-    return view('show', ['task' =>$task]);
 })->name('tasks.show');
 
 // Route::get('/xxx', function () {

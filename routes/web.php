fefe1722d6a0a6ea87da4cc4ba1b4545b20d0a1c
Your Name <nihaal@sapnagroup.com>
2024-01-23 09:18:11 +0530

    Toggling task state

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/tasks/{task}/edit', function (Task $task) {
     'task' => $task
   ]);
 
-})->name('tasks.show');
+})->name('tasks.edit');
 
 Route::get('/tasks/{task}', function (Task $task) {
 
@@ -67,6 +67,11 @@ Route::delete('/tasks/{task}/', function (Task $task) {
   return redirect()->route('tasks.index')
   ->with('success', 'Task deleted successfully');
 })->name('tasks.destroy');
+
+Route::put('/tasks/{task}/toggle-complete', function (Task $task) {
+  $task->toggleComplete();
+  return redirect()->back()->with('success', 'Task updated successfully');
+})->name('tasks.toggle-complete');
 
 // Route::get('/xxx', function () {
 //     return 'Hello';
